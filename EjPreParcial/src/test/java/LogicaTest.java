aeb65e6ca40293f7fc81a55aced551cf4e3b44f8
Matias <matiascastellano93@gmail.com>
2025-06-19 18:50:02 -0300

    proyecto Test

---
+++ EjPreParcial/src/test/java/LogicaTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LogicaTest {
+
+}
